531af4b8fa2de925fee3e6a6ab17dcd45e85c07f
nslebruh <37894360+nslebruh@users.noreply.github.com>
2022-12-24 15:43:12 +1100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Fluid {
+    id: String,
+    name: String,
+    amount: u32,
+    temperature: i32
+}
+
+#[derive(Serialize, Deserialize)]
+struct Item {
+    id: String,
+    name: String,
+    amount: u32
+}
+
+#[derive(Serialize, Deserialize)]
+struct RecipeThings {
+    items: Vec<Item>,
+    fluids: Vec<Fluid>
+}
+
+#[derive(Serialize, Deserialize)]
+struct Machine {
+    voltage_tier: String,
+    _type: String,
+
+}
+
+#[derive(Serialize, Deserialize)]
+struct GTNHRecipe {
+    input: Vec<RecipeThings>,
+    output: Vec<RecipeThings>,
+    machine: Machine,
+    ticks: u32,
+    eu_per_tick: u32,
+    total_eu: u32,
+    multiblock: bool,
+    requires_cleanroom: bool
+
+}
+
+fn main() {
+    println!("Hello, world!");
+}
